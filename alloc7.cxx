1383e49106467695087a139e6480a713446d7de1
Sean Baxter <lightborn@gmail.com>
2020-02-28 19:43:29 -0500

    Printing values in alloc7

--- alloc7.cxx
+++ alloc7.cxx
@@ -5,7 +5,7 @@ struct obj_t {
   static logging_resource_t logger;
 
   pmr::vector<int> a = { 1, 2, 3, 4 } using(logger);
-  pmr::vector<int> b = { 5, 6 } using(logger);
+  pmr::vector<int> b = { 5, 6, 7, 8, 9 } using(logger);
 };
 
 logging_resource_t obj_t::logger { };
@@ -19,10 +19,12 @@ int main() {
 
     // By omission, select the member-initializer for obj_t::b.
   };
+  printf("%d ", obj.a[:])...; printf("\n");
+  printf("%d ", obj.b[:])...; printf("\n");
 
   printf("&logger_local: %p\n", &logger_local);
   printf("obj.a's resource: %p\n", obj.a.get_allocator().resource());
-  
+
   printf("&obj_t::logger: %p\n", &obj_t::logger);
   printf("obj.b's resource: %p\n", obj.b.get_allocator().resource());
 }
